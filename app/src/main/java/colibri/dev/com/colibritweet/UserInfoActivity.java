1c33b879367f6632b71e51b1bc0aac6e31b90aeb
Colibridev <it.mobile.learning@gmail.com>
2018-01-08 20:56:00 +0300

    lesson9

--- app/src/main/java/colibri/dev/com/colibritweet/UserInfoActivity.java
+++ app/src/main/java/colibri/dev/com/colibritweet/UserInfoActivity.java
@@ -1,21 +1,68 @@
 package colibri.dev.com.colibritweet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-public class UserInfoActivity extends AppCompatActivity {
+import colibri.dev.com.colibritweet.pojo.User;
 
+public class UserInfoActivity extends AppCompatActivity {
     private ImageView userImageView;
+    private TextView nameTextView;
+    private TextView nickTextView;
+    private TextView descriptionTextView;
+    private TextView locationTextView;
+    private TextView followingCountTextView;
+    private TextView followersCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-        userImageView = findViewById(R.id.user_image_view);
 
-        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(userImageView);
+        userImageView = findViewById(R.id.user_image_view);
+        nameTextView = findViewById(R.id.user_name_text_view);
+        nickTextView = findViewById(R.id.user_nick_text_view);
+        descriptionTextView = findViewById(R.id.user_description_text_view);
+        locationTextView = findViewById(R.id.user_location_text_view);
+        followingCountTextView = findViewById(R.id.following_count_text_view);
+        followersCountTextView = findViewById(R.id.followers_count_text_view);
+
+        loadUserInfo();
+    }
+
+    private void loadUserInfo() {
+        User user = getUser();
+        displayUserInfo(user);
+    }
+
+    private void displayUserInfo(User user) {
+        Picasso.with(this).load(user.getImageUrl()).into(userImageView);
+        nameTextView.setText(user.getName());
+        nickTextView.setText(user.getNick());
+        descriptionTextView.setText(user.getDescription());
+        locationTextView.setText(user.getLocation());
+
+        String followingCount = String.valueOf(user.getFollowingCount());
+        followingCountTextView.setText(followingCount);
+
+        String followersCount = String.valueOf(user.getFollowersCount());
+        followersCountTextView.setText(followersCount);
+    }
+
+    private User getUser() {
+        return new User(
+                1L,
+                "http://i.imgur.com/DvpvklR.png",
+                "DevColibri",
+                "devcolibri",
+                "Sample description",
+                "USA",
+                42,
+                42
+        );
     }
 }
---
+++ app/src/main/java/colibri/dev/com/colibritweet/pojo/User.java
@@ -0,0 +1,95 @@
+package colibri.dev.com.colibritweet.pojo;
+
+public class User {
+    private long id;
+    private String imageUrl;
+    private String name;
+    private String nick;
+    private String description;
+    private String location;
+    private int followingCount;
+    private int followersCount;
+
+    public User(long id,
+                String imageUrl,
+                String name,
+                String nick,
+                String description,
+                String location,
+                int followingCount,
+                int followersCount) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.nick = nick;
+        this.description = description;
+        this.location = location;
+        this.followingCount = followingCount;
+        this.followersCount = followersCount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getFollowingCount() {
+        return followingCount;
+    }
+
+    public int getFollowersCount() {
+        return followersCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (followingCount != user.followingCount) return false;
+        if (followersCount != user.followersCount) return false;
+        if (imageUrl != null ? !imageUrl.equals(user.imageUrl) : user.imageUrl != null)
+            return false;
+        if (!name.equals(user.name)) return false;
+        if (!nick.equals(user.nick)) return false;
+        if (description != null ? !description.equals(user.description) : user.description != null) {
+            return false;
+        }
+        return location != null ? location.equals(user.location) : user.location == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
+        result = 31 * result + name.hashCode();
+        result = 31 * result + nick.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (location != null ? location.hashCode() : 0);
+        result = 31 * result + followingCount;
+        result = 31 * result + followersCount;
+        return result;
+    }
+}
