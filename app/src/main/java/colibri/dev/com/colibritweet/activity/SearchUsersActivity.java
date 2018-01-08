6e0a79019a1e44490461075738408754218613c1
Colibridev <it.mobile.learning@gmail.com>
2018-01-08 20:57:34 +0300

    lesson14

--- app/src/main/java/colibri/dev/com/colibritweet/activity/SearchUsersActivity.java
+++ app/src/main/java/colibri/dev/com/colibritweet/activity/SearchUsersActivity.java
@@ -8,6 +8,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import colibri.dev.com.colibritweet.R;
 import colibri.dev.com.colibritweet.adapter.UsersAdapter;
@@ -16,6 +24,9 @@ import colibri.dev.com.colibritweet.pojo.User;
 public class SearchUsersActivity extends AppCompatActivity {
     private RecyclerView usersRecyclerView;
     private UsersAdapter usersAdapter;
+    private Toolbar toolbar;
+    private EditText queryEditText;
+    private Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +34,62 @@ public class SearchUsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_users);
         initRecyclerView();
 
-        searchUsers();
+        toolbar = findViewById(R.id.toolbar);
+        queryEditText = toolbar.findViewById(R.id.query_edit_text);
+        searchButton = toolbar.findViewById(R.id.search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchUsers();
+            }
+        });
+
+        queryEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    searchUsers();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initRecyclerView() {
+        usersRecyclerView = findViewById(R.id.users_recycler_view);
+        usersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        UsersAdapter.OnUserClickListener onUserClickListener = new UsersAdapter.OnUserClickListener() {
+            @Override
+            public void onUserClick(User user) {
+                Intent intent = new Intent(SearchUsersActivity.this, UserInfoActivity.class);
+                intent.putExtra(UserInfoActivity.USER_ID, user.getId());
+                startActivity(intent);
+            }
+        };
+        usersAdapter = new UsersAdapter(onUserClickListener);
+        usersRecyclerView.setAdapter(usersAdapter);
     }
 
     private void searchUsers() {
         Collection<User> users = getUsers();
+        usersAdapter.clearItems();
         usersAdapter.setItems(users);
     }
 
@@ -55,21 +117,5 @@ public class SearchUsersActivity extends AppCompatActivity {
                         13
                 )
         );
-    }
-
-    private void initRecyclerView() {
-        usersRecyclerView = findViewById(R.id.users_recycler_view);
-        usersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        UsersAdapter.OnUserClickListener onUserClickListener = new UsersAdapter.OnUserClickListener() {
-            @Override
-            public void onUserClick(User user) {
-                Intent intent = new Intent(SearchUsersActivity.this, UserInfoActivity.class);
-                intent.putExtra(UserInfoActivity.USER_ID, user.getId());
-                startActivity(intent);
-            }
-        };
-        usersAdapter = new UsersAdapter(onUserClickListener);
-        usersRecyclerView.setAdapter(usersAdapter);
     }
 }
