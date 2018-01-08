8408510013ccef26b0bde6573cb12de2eaf8888c
Colibridev <it.mobile.learning@gmail.com>
2018-01-08 20:54:58 +0300

    lesson6

--- app/src/main/java/colibri/dev/com/colibritweet/UserInfoActivity.java
+++ app/src/main/java/colibri/dev/com/colibritweet/UserInfoActivity.java
@@ -2,12 +2,20 @@ package colibri.dev.com.colibritweet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    private ImageView userImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        userImageView = findViewById(R.id.user_image_view);
+
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(userImageView);
     }
 }
